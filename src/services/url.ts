4905be16075619ddbe781c54cad25d36ae4cf8c3
ron12341 <arvincruz7524@gmail.com>
2025-07-29 20:30:27 -0500

    minor changes

--- src/services/url.ts
+++ src/services/url.ts
@@ -1,10 +1,10 @@
 // current weather
-export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 // Direct geocoding
-export const OPTIONS_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+export const OPTIONS_URL = "https://api.openweathermap.org/geo/1.0/direct?q=";
 
 // 5 day / 3 hour
-export const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+export const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 
-export const ICON_URL = 'http://openweathermap.org/img/wn/';
+export const ICON_URL = "http://openweathermap.org/img/wn/";
